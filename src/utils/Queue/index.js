f7ec5e88ce365571946e81032cd9d8c89f524c4b
chris-nee <lingngeehung@gmail.com>
2022-08-07 13:57:02 +0800

    add: code comments for class implementation of queue

--- src/utils/Queue/index.js
+++ src/utils/Queue/index.js
@@ -1,23 +1,28 @@
 /*
+ * Class for Queue Implementation
+ *
  * JS has no native Queue data structure.
- * So this is a simple implementation.
+ * So this is a simple class implementation of Queue
  *
  * - Last in first out principle
  * - Enqueue O(1) , add new to back of queue
  * - Dequeue O(1) , pop out from the front of queu
  *
+ *   @property {Object} storage     - map for storing index to value
+ *   @property {number} firstIndex  - index holding the next value to dequeue
+ *   @property {number} lastIndex   - index to add the next incoming value
  */
 class Queue {
   #storage;
   #firstIndex;
   #lastIndex;
 
+  /*
+   * Creates a Queue, initialises private properties
+   *
+   * @param {any []} array - The array to initiate into a queue
+   */
   constructor(arr = []) {
-    /*
-     * storage private
-     * firstIndex private
-     * lastIndex p
-     */
     this.#storage = {};
     this.#firstIndex = 0;
     this.#lastIndex = 0;
@@ -26,10 +31,20 @@ class Queue {
       this.enqueue(v);
     });
   }
+
+  /*
+   * Adds a new value to the back of the queue, increases lastIndex pointer by 1
+   * @param {any} value - Value to add to the end of the queue
+   */
   enqueue(value) {
     this.#storage[this.#lastIndex] = value;
     this.#lastIndex += 1;
   }
+
+  /*
+   * Removes value from the front of the queue, increases firstIndex pointer by 1
+   * @return {any}      - The value at the front of the queue
+   */
   dequeue() {
     if (this.getSize() === 0) {
       throw new Error("Queue is empty");
@@ -39,6 +54,11 @@ class Queue {
     this.#firstIndex += 1;
     return toReturn;
   }
+
+  /*
+   * Calculates and return the length of the queue
+   * @return {number}      - The current length of the queue
+   */
   getSize() {
     return this.#lastIndex - this.#firstIndex;
   }
