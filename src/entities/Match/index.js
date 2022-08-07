1dbf147ecbb4efbe1713a3b9d2cf2fe13c678399
chris-nee <lingngeehung@gmail.com>
2022-08-07 20:50:05 +0800

    change: formatted match details for more compact info

--- src/entities/Match/index.js
+++ src/entities/Match/index.js
@@ -36,18 +36,46 @@ class Match {
   }
 
   /*
-   * Returns a formatted string of match details 
+   * Returns a formatted string of match details
    * @return {string}               - Formatted string of match details
    */
   getMatchDetails() {
+    const reduceStats = (prev, player, _, arr) => {
+      const wins = player.getWins();
+      const losses = player.getLosses();
+      const winLossRatio = player.getWinLoseRatio();
+
+      prev.totalWins += wins;
+      prev.totalLosses += losses;
+      prev.averageWinLossRatio += winLossRatio / arr.length;
+      return prev;
+    };
+
+    const t1Stats = this.#team1.reduce(reduceStats, {
+      totalLosses: 0,
+      totalWins: 0,
+      averageWinLossRatio: 0,
+    });
+    const t2Stats = this.#team2.reduce(reduceStats, {
+      totalLosses: 0,
+      totalWins: 0,
+      averageWinLossRatio: 0,
+    });
+
     return `
         ==========================
         Match Formed
+        ==========================
         TEAM 1 :
+        Total Wins: ${t1Stats.totalWins} , Total Losses: ${
+      t1Stats.totalLosses
+    } , Average Win / Loss Ratio: ${t1Stats.averageWinLossRatio}
         ${this.#team1.map((player) => player.getPlayerStats()).join("")}
         --------------------------
-
         TEAM 2 :
+        Total Wins: ${t2Stats.totalWins} , Total Losses: ${
+      t2Stats.totalLosses
+    } , Average Win / Loss Ratio: ${t2Stats.averageWinLossRatio}
         ${this.#team2.map((player) => player.getPlayerStats()).join("")}
         ==========================
     `;
