043deeaaeae8eb17921a298592cac0136e86567f
chris-nee <lingngeehung@gmail.com>
2022-08-07 14:35:53 +0800

    add: code comments for class implementation of Match

--- src/entities/Match/index.js
+++ src/entities/Match/index.js
@@ -1,20 +1,44 @@
+/*
+ * Class for Match
+ *
+ * @private @property {Player[]} team1      - Array of players in team 1
+ * @private @property {Player[]} team2      - Array of players in team 2
+ */
+
 class Match {
   #team1;
   #team2;
 
+  /*
+   * Creates a Match
+   * @params {Player[]} team1       - Array of players in team 1
+   * @params {Player[]} team2       - Array of players in team 2
+   */
   constructor(team1, team2) {
     this.#team1 = team1;
     this.#team2 = team2;
   }
 
+  /*
+   * Returns the array of players in team 1
+   * @return {Player[]}             - Array of players in team 1
+   */
   getTeam1() {
     return this.#team1;
   }
 
+  /*
+   * Returns the array of players in team 2
+   * @return {Player[]}             - Array of players in team 2
+   */
   getTeam2() {
     return this.#team2;
   }
 
+  /*
+   * Returns a formatted string of match details 
+   * @return {string}               - Formatted string of match details
+   */
   getMatchDetails() {
     return `
         ==========================
