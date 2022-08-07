9289b5d894c896ee4cb926085066b9401ff1ceb3
chris-nee <lingngeehung@gmail.com>
2022-08-07 14:42:21 +0800

    add: code comments for class implementation of Player

--- src/entities/Player/index.js
+++ src/entities/Player/index.js
@@ -1,16 +1,39 @@
+/*
+ * Class for Player
+ *
+ * @private @property {string} name         - Name of player
+ * @private @property {number} wins         - Matches won by player
+ * @private @property {number} losses       - Matches lost by player
+ */
 class Player {
   #name;
   #wins;
   #losses;
 
+  /*
+   * Checks if name value is a valid string
+   * @param  {string} name          - Name input to be checked
+   * @return {boolean}              - Boolean for whether name is valid
+   */
   static validName(name) {
     return String(name) === name;
   }
 
+  /*
+   * Checks if count value is a valid number
+   * @param  {number} name          - count input to be checked
+   * @return {boolean}              - Boolean for whether count is valid
+   */
   static validCount(count) {
     return +count === count;
   }
 
+  /*
+   * Creates a Player
+   * @params {string} name          - Name of player
+   * @params {number} wins          - Matches won by player
+   * @params {number} losses        - Matches lost by player
+   */
   constructor(name = "", wins = 0, losses = 0) {
     if (!Player.validName(name)) throw TypeError("Invalid Name");
     if (!Player.validCount(wins)) throw TypeError("Invalid wins count");
@@ -21,22 +44,42 @@ class Player {
     this.#losses = losses;
   }
 
+  /*
+   * Returns name of player
+   * @return {string} this.#name    - Name of player
+   */
   getName() {
     return this.#name;
   }
 
+  /*
+   * Returns matches won by player
+   * @return {number} this.#wins    - Matches won by player
+   */
   getWins() {
     return this.#wins;
   }
 
+  /*
+   * Returns matches lost by player
+   * @return {number} this.#losses  - Matches lost by player
+   */
   getLosses() {
     return this.#losses;
   }
 
+  /*
+   * Calculate and returns win lose ratio of player
+   * @return {number}               - Wins / Lost ratio of player
+   */
   getWinLoseRatio() {
     return this.getWins() / this.getLosses();
   }
 
+  /*
+   * Returns a formated string of player details
+   * @return {string}               - Formatted string of player details
+   */
   getPlayerStats() {
     return `
         &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
