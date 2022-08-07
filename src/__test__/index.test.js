b5290fcc49053300eb6d8256b4565699604fa159
chris-nee <lingngeehung@gmail.com>
2022-08-07 20:31:50 +0800

    add: integration testing for system

--- src/__test__/index.test.js
+++ src/__test__/index.test.js
@@ -1,21 +1,36 @@
 import DataJson from "./data.json";
-import { Player } from "#entities/index.js";
-import { MatchMaker } from "#entities/index.js";
+import { Player, MatchMaker } from "#entities/index.js";
+
+function randomNumInRange(min, max) {
+  const range = Math.random() * (max - min);
+  return Math.floor(range + min);
+}
 
 describe("Match making process", () => {
   test("Match making", () => {
     const matchMaker = new MatchMaker();
-    DataJson.forEach((playerData) => {
-      const { name, wins, losses } = playerData;
-      matchMaker.enterMatchMaking(new Player(name, wins, losses));
+
+    const players = DataJson.map(
+      ({ name, wins, losses }) => new Player(name, wins, losses)
+    );
+    players.forEach((player) => {
+      matchMaker.enterMatchMaking(player);
     });
 
-    function randomNumInRange(min, max) {
-      const range = Math.random() * (max - min);
-      return Math.floor(range + min);
-    }
+    expect(matchMaker.getNumOfPlayersInQueue()).toBe(DataJson.length);
 
-    const match = matchMaker.findMatch(randomNumInRange(3, 5));
+    const teamSize = 4;
+    const match = matchMaker.findMatch(teamSize);
     expect(match).toBeTruthy();
+
+    const matchTeam1 = match.getTeam1();
+    expect(matchTeam1.length).toBe(teamSize);
+
+    const matchTeam2 = match.getTeam2();
+    expect(matchTeam2.length).toBe(teamSize);
+
+    /* First n players who entered match making will start a match first */
+    const allPlayersInMatch = matchTeam1.concat(matchTeam2);
+    expect(allPlayersInMatch).toEqual(players.slice(0, teamSize * 2));
   });
 });
